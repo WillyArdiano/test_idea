3084a933ec289c105b1ce1e1e19cb6a3e9c652d4
WillyArdiano <willyjessiel99@gmail.com>
2022-04-13 11:24:15 -0600

    Inicializando proyecto

---
+++ src/main/java/com/example/test_idea/TestIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestIdeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_idea/TestIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_idea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestIdeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
